d4da7e58c6a62616f871fb5a2ac574b40b2bd6ba
HyCraftHD <hycrafthd@live.de>
2021-08-01 12:08:26 +0200

    Update load config

--- load-config.groovy
+++ load-config.groovy
@@ -1,13 +1,10 @@
-def loadConfig() {
+ext.loadConfig = {
+	->
 	ext.buildProps = file "build.properties"
-
+	
 	buildProps.withReader {
 		def prop = new Properties()
 		prop.load(it)
 		ext.config = new ConfigSlurper().parse prop
 	}
-}
-
-ext {
-	loadConfig = this.&loadConfig
 }
