6e3fc6ab517e4f7438ac226458571593ec887e4c
HyCraftHD <hycrafthd@live.de>
2021-08-01 12:12:07 +0200

    Update tag release script

--- tag-release.groovy
+++ tag-release.groovy
@@ -1,9 +1,9 @@
 task tagRelease {
 	doLast {
 		validateBuild()
-    		def tagName = "${config.forge.mcversion}-${project.version}"
+		def tagName = "${config.forge.mcversion}-${project.version}"
 		println "Tag release commit with tag ${tagName}"
 		execute("git tag ${tagName}")
 		execute("git push origin ${tagName}")
- 	}
+	}
 }
