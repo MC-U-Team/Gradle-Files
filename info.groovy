dc48fc1e7dcf9fa25aabfc8eb3b6f25f8686d6c0
HyCraftHD <hycrafthd@live.de>
2021-08-01 12:07:32 +0200

    Update info groovy

---
+++ info.groovy
@@ -0,0 +1,4 @@
+ext.printInfo = {
+	->
+	println("Java: " + System.getProperty("java.version") + " JVM: " + System.getProperty("java.vm.version") + "(" + System.getProperty("java.vendor") + ") Arch: " + System.getProperty("os.arch"))
+}
