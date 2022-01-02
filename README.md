c431e42be6936c281ec3ed46331dea4b9346851e
HyCraftHD <hycrafthd@live.de>
2022-01-02 03:55:44 +0100

    Remove old files, start project as plugin now

--- README.md
+++
@@ -1,8 +0,0 @@
-# Useful Gradle Files for Minecraft Modding
-
-Some features
-
- - Generate build numbers
- - Utility methods
- - Sign Jars
- - Tag Release Commits
--- info.groovy
+++
@@ -1,6 +0,0 @@
-ext.printInfo = {
-	->
-	println "Java: " + System.getProperty("java.version")
-	println "JVM: " + System.getProperty("java.vm.version") + "(" + System.getProperty("java.vendor") + ")"
-	println "Arch: " + System.getProperty("os.arch")
-}
