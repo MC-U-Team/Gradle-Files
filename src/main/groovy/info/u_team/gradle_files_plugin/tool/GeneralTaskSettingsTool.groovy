3d77b19b7886c95f50b400b9093393782feb0db4
HyCraftHD <hycrafthd@live.de>
2022-01-04 04:47:00 +0100

    Set encoding to utf8

--- src/main/groovy/info/u_team/gradle_files_plugin/tool/GeneralTaskSettingsTool.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/tool/GeneralTaskSettingsTool.groovy
@@ -3,15 +3,22 @@ package info.u_team.gradle_files_plugin.tool
 import javax.tools.JavaCompiler
 
 import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.javadoc.Javadoc
 
+import groovy.json.JsonGenerator.Options
 import info.u_team.gradle_files_plugin.GradleFilesPlugin
 
 class GeneralTaskSettingsTool {
 	
 	static void setup(final GradleFilesPlugin plugin) {
-		//
+		// Set utf 8 as default encoding for compile tasks
 		plugin.project.tasks.withType(JavaCompile) { task ->
-			println(task)
+			task.options.encoding = "UTF-8"
+		}
+		
+		// Set utf 8 as default encoding for javadoc tasks
+		plugin.project.tasks.withType(Javadoc) { task ->
+			task.options.encoding = "UTF-8"
 		}
 	}
 }
