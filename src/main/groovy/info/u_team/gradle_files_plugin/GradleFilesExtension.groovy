efda5bc52b124076a3976bb21e9183c8865038e2
HyCraftHD <hycrafthd@live.de>
2022-01-04 16:36:04 +0100

    Add some docs

--- src/main/groovy/info/u_team/gradle_files_plugin/GradleFilesExtension.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/GradleFilesExtension.groovy
@@ -1,16 +1,39 @@
 package info.u_team.gradle_files_plugin
 
+import org.gradle.jvm.tasks.Jar
+
+import info.u_team.gradle_files_plugin.extension.DefaultJarExtensionImpl
 import info.u_team.gradle_files_plugin.extension.DefaultManifestExtensionImpl
 import info.u_team.gradle_files_plugin.extension.ForgeDependencyExtensionImpl
 
 class GradleFilesExtension {
 	
+	/**
+	 * Should be set to configure the vendor of the build
+	 */
 	String vendor
 	
+	/**
+	 * Returns a manifest default file
+	 * @return Manifest
+	 */
 	def defaultManifest() {
 		DefaultManifestExtensionImpl.defaultManifest()
 	}
 	
+	/**
+	 * Include license file to build
+	 * Exclude .cache file	
+	 * @param task Jar task
+	 */
+	void defaultJar(Jar task) {
+		DefaultJarExtensionImpl.defaultJar(task)
+	}
+	
+	/**
+	 * Return the forge dependency
+	 * @return Forge dependency
+	 */
 	def forgeDependency() {
 		ForgeDependencyExtensionImpl.forgeDependency()
 	}
---
+++ src/main/groovy/info/u_team/gradle_files_plugin/extension/DefaultJarExtensionImpl.groovy
@@ -0,0 +1,11 @@
+package info.u_team.gradle_files_plugin.extension
+
+import org.gradle.jvm.tasks.Jar
+
+class DefaultJarExtensionImpl {
+	
+	static void defaultJar(Jar task) {
+		task.from("LICENSE")
+		task.exclude(".cache")
+	}
+}
