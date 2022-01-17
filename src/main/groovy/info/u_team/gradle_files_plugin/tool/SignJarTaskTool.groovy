4e28d02fef644f570f64e92b603d112ea0f3e0f4
HyCraftHD <hycrafthd@live.de>
2022-01-17 04:47:39 +0100

    Remove L
    Signed-off-by: HyCraftHD <hycrafthd@live.de>

--- src/main/groovy/info/u_team/gradle_files_plugin/tool/SignJarTaskTool.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/tool/SignJarTaskTool.groovy
@@ -8,6 +8,8 @@ import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.plugins.jvm.internal.JvmPluginExtension
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.internal.impldep.org.jetbrains.annotations.Nls.Capitalization
+import org.gradle.internal.logging.progress.ProgressLogger
+import org.gradle.internal.logging.progress.ProgressLoggerFactory
 
 import info.u_team.gradle_files_plugin.Constants
 import info.u_team.gradle_files_plugin.GradleFilesPlugin
@@ -15,31 +17,33 @@ import net.minecraftforge.gradle.common.tasks.SignJar
 import net.minecraftforge.gradle.userdev.tasks.RenameJarInPlace
 
 class SignJarTaskTool {
-	
+
 	private static final def requiredProperties = [
 		Constants.KEYSTORE,
 		Constants.KEYSTORE_ALIAS,
 		Constants.KEYSTORE_PASSWORD
 	]
-	
+
 	static void add(final GradleFilesPlugin plugin) {
 		if(!plugin.extension.signJars) {
 			return
 		}
 		final def project = plugin.project
-		
+
 		if(!project.properties.keySet().containsAll(requiredProperties)) {
-			project.logger.warn("Signing of jars was requested, but required properties are missing")
+			plugin.project.gradle.sharedServices.properties
+			
+						project.logger.warn("Signing of jars was requested, but required properties are missing")
 			return
 		}
-		
+
 		//		project.configurations.getByName("archives").allArtifacts.first()
 		//
 		//		project.components.getByName("java").art
 		//
 		//		NamedDomainObjectContainer<RenameJarInPlace> reobf = null
 		//		reobf.create("")
-		
+
 		project.tasks.withType(Jar) { jarTask ->
 			final def signJarTask = project.tasks.register("sign" + StringUtils.capitalize(jarTask.name), SignJar) { task ->
 				task.description = "Sign the jar ${jarTask.name}"
