1e1db20c813f958e90c2f12314a4e1a2c3a070b0
HyCraftHD <hycrafthd@live.de>
2022-01-20 13:32:23 +0100

    Git add maven pom filter. Unfortunately we cannot reuse the ones from the gradle metadata, as the publication is really stupid to get in the maven pom, and the content is populated as soon as one property is queried. Other plugins or gradle changes can break therefore the assumption.

--- src/main/groovy/info/u_team/gradle_files_plugin/tool/RemovedMappedDependenciesTool.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/tool/RemovedMappedDependenciesTool.groovy
@@ -1,22 +1,38 @@
 package info.u_team.gradle_files_plugin.tool
 
+import java.lang.reflect.Field
+
+import org.gradle.api.publish.maven.internal.publication.DefaultMavenPom
+import org.gradle.api.publish.maven.internal.publication.MavenPomInternal
 import org.gradle.api.publish.maven.internal.publication.MavenPublicationInternal
+import org.gradle.api.publish.maven.tasks.GenerateMavenPom
 import org.gradle.api.publish.tasks.GenerateModuleMetadata
 
 import info.u_team.gradle_files_plugin.GradleFilesPlugin
+import info.u_team.gradle_files_plugin.util.DependencyFilteredMavenPomInternal
 import info.u_team.gradle_files_plugin.util.DependencyFilteredMavenPublicationInternal
 
 class RemovedMappedDependenciesTool {
-
+	
 	static void remove(final GradleFilesPlugin plugin) {
+		plugin.project.tasks.withType(GenerateMavenPom) { task ->
+			final def pom = task.pom
+			if(pom instanceof MavenPomInternal) {
+				final def internalPom = pom as MavenPomInternal
+				final def filteredPom = new DependencyFilteredMavenPomInternal(internalPom)
+				
+				task.pom = filteredPom
+			}
+		}
+		
 		plugin.project.tasks.withType(GenerateModuleMetadata) { task ->
-			final def publication = task.getPublication().get()
+			final def publication = task.publication.get()
 			if(publication instanceof MavenPublicationInternal) {
 				final def internalPublication = publication as MavenPublicationInternal
 				final def filteredPublication = new DependencyFilteredMavenPublicationInternal(internalPublication)
-
-				task.getPublication().set(filteredPublication)
-				task.getPublications().set([filteredPublication])
+				
+				task.publication.set(filteredPublication)
+				task.publications.set([filteredPublication])
 			}
 		}
 	}
