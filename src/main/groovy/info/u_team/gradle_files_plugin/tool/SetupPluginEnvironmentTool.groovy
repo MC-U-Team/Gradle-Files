9d658830b1cf156e60976d594960693fe995710f
HyCraftHD <hycrafthd@live.de>
2023-03-12 21:35:34 +0100

    Do use plugin context

--- src/main/groovy/info/u_team/gradle_files_plugin/tool/SetupPluginEnvironmentTool.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/tool/SetupPluginEnvironmentTool.groovy
@@ -28,17 +28,13 @@ class SetupPluginEnvironmentTool {
 		// Add value methods
 		final def extraProperties = plugin.project.extensions.extraProperties
 		
-		extraProperties.defaultPropertyValue = this.&defaultPropertyValue
-		extraProperties.propertyValue = this.&propertyValue
-	}
-	
-	private static def defaultPropertyValue(def name) {
-		final def property = propertyValue(name)
-		return property != null ? property : "noValue"
-	}
-	
-	private static def propertyValue(def name) {
-		final def env = System.getenv(name)
-		return env != null ? env : GradleFilesPlugin.getInstance().project.findProperty(name)
+		extraProperties.defaultPropertyValue = { name ->
+			final def property = extraProperties.propertyValue(name)
+			return property != null ? property : "noValue"
+		}
+		extraProperties.propertyValue = { name ->
+			final def env = System.getenv(name)
+			return env != null ? env : plugin.project.findProperty(name)
+		}
 	}
 }
