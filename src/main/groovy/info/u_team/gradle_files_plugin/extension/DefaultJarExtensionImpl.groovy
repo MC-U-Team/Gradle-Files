7127213e31061b88dd2137b38528c6ea2c9c850a
HyCraftHD <hycrafthd@live.de>
2023-03-27 00:06:17 +0200

    Use mainproject for license lookup

--- src/main/groovy/info/u_team/gradle_files_plugin/extension/DefaultJarExtensionImpl.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/extension/DefaultJarExtensionImpl.groovy
@@ -4,11 +4,14 @@ import org.gradle.api.Project
 import org.gradle.jvm.tasks.Jar
 
 import info.u_team.gradle_files_plugin.GradleFilesExtension
+import info.u_team.gradle_files_plugin.util.GradleFilesUtil
 
 class DefaultJarExtensionImpl {
 	
 	static void defaultJar(final Project project, final GradleFilesExtension extension, Jar task) {
-		task.from("LICENSE")
+		final def mainProject = GradleFilesUtil.getMainProject(project)
+		
+		task.from(mainProject.file("LICENSE"))
 		task.exclude(".cache")
 		
 		task.manifest(DefaultManifestExtensionImpl.defaultManifest(project, extension))
