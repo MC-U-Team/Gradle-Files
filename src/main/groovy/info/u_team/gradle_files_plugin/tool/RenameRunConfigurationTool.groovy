0a635eaa7581e96ce8c16ca9a4a7aa612e61443e
HyCraftHD <hycrafthd@live.de>
2023-04-09 21:34:12 +0200

    Use eclipse project name for forge run configuration

--- src/main/groovy/info/u_team/gradle_files_plugin/tool/RenameRunConfigurationTool.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/tool/RenameRunConfigurationTool.groovy
@@ -1,13 +1,20 @@
 package info.u_team.gradle_files_plugin.tool
 
+import org.gradle.plugins.ide.eclipse.model.EclipseModel
+
 import info.u_team.gradle_files_plugin.GradleFilesPlugin
 
 class RenameRunConfigurationTool {
 	
 	static void rename(final GradleFilesPlugin plugin) {
-		final def minecraftExtension = plugin.project.extensions.findByName("minecraft")
+		final def project = plugin.project
+		final def minecraftExtension = project.extensions.findByName("minecraft")
 		minecraftExtension?.runs?.each { run ->
-			run.taskName = "${plugin.project.name}_${run.taskName}"
+			def projectName = (project.extensions.findByName("eclipse") as EclipseModel)?.project?.name
+			if(!projectName) {
+				projectName = project.name
+			}
+			run.taskName = "${projectName}_${run.taskName}"
 		}
 	}
 }
