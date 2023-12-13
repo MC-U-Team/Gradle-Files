a0d8faf83fabf0bd35b317d434cda0f929c96c98
HyCraftHD <hycrafthd@live.de>
2023-12-13 15:37:36 +0100

    Add sign jar task

---
+++ src/main/groovy/info/u_team/sign_jar_plugin/task/SignJarTask.groovy
@@ -0,0 +1,45 @@
+package info.u_team.sign_jar_plugin.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
+
+@DisableCachingByDefault
+abstract class SignJarTask extends DefaultTask {
+
+	@TaskAction
+	void sign() {
+		final def properties = [
+			keyStore: getKeyStore().getOrElse(""),
+			alias: getAlias().get(),
+			storepass: getStorePass().get(),
+			keyPass: getKeyPass().getOrElse(""),
+			jar: getInputFile().get().getAsFile(),
+			signedJar: getOutputFile().get().getAsFile()
+		]
+		ant.invokeMethod("signjar", properties)
+	}
+
+	@InputFile
+	abstract RegularFileProperty getInputFile();
+
+	@Internal
+	abstract RegularFileProperty getOutputFile();
+
+	@Input
+	abstract Property<String> getKeyStore()
+
+	@Input
+	abstract Property<String> getAlias()
+
+	@Input
+	abstract Property<String> getStorePass()
+
+	@Input
+	abstract Property<String> getKeyPass()
+}
---
+++ src/main/java/info/u_team/sign_jar_plugin/SignJarPlugin.java
@@ -0,0 +1,12 @@
+package info.u_team.sign_jar_plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class SignJarPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+	}
+
+}
