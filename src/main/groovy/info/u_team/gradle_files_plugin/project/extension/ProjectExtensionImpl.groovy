43fffc32bed01af9228e31648f18862bb0dd39dc
HyCraftHD <hycrafthd@live.de>
2023-12-16 02:08:16 +0100

    Add compile static annotations

--- src/main/groovy/info/u_team/gradle_files_plugin/project/extension/ProjectExtensionImpl.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/project/extension/ProjectExtensionImpl.groovy
@@ -1,10 +1,11 @@
 package info.u_team.gradle_files_plugin.project.extension
 
-import org.apache.commons.lang3.StringUtils
 import org.gradle.api.Project
 
+import groovy.transform.CompileStatic
 import info.u_team.gradle_files_plugin.util.NamingUtil
 
+@CompileStatic
 class ProjectExtensionImpl {
 
 	static Project project(final Project project, final String name) {
--- src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
@@ -2,6 +2,9 @@ package info.u_team.gradle_files_plugin.util
 
 import org.apache.commons.lang3.StringUtils
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class NamingUtil {
 
 	static String subProjectName(final String rootProjectName, String projectName) {
