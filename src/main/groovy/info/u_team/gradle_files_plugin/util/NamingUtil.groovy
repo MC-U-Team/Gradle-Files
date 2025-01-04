843905ac94eba88d9092ff0b343870eff196f8b0
HyCraftHD <hycrafthd@live.de>
2025-01-04 13:28:29 +0100

    Fix not passing values to project name & capizalize each split word

--- src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
@@ -1,15 +1,18 @@
 package info.u_team.gradle_files_plugin.util
 
-import org.apache.commons.lang3.RegExUtils
 import org.apache.commons.lang3.StringUtils
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class NamingUtil {
-
+	
+	private static final String INVALID_CHARACTERS = "[\\\\\\/\\:\\<\\>\\\"\\?\\*\\|]"
+	
 	static String subProjectName(final String rootProjectName, final String projectName) {
-		final def sanitizedString = RegExUtils.replaceAll(projectName, "[\\\\\\/\\:\\<\\>\\\"\\?\\*\\|]", "_")
-		return "${rootProjectName}_${StringUtils.capitalize(projectName)}"
+		final def sanitizedString = projectName.split(INVALID_CHARACTERS).collect { namePart ->
+			StringUtils.capitalize(namePart)
+		}.join("_")
+		return "${rootProjectName}_${StringUtils.capitalize(sanitizedString)}"
 	}
 }
