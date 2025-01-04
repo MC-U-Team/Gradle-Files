7db2d59e9fe397bc6fc6b9afeebcc58177e67b26
HyCraftHD <hycrafthd@live.de>
2025-01-04 12:28:18 +0100

    Sanitize name for project

--- src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
+++ src/main/groovy/info/u_team/gradle_files_plugin/util/NamingUtil.groovy
@@ -1,5 +1,6 @@
 package info.u_team.gradle_files_plugin.util
 
+import org.apache.commons.lang3.RegExUtils
 import org.apache.commons.lang3.StringUtils
 
 import groovy.transform.CompileStatic
@@ -7,7 +8,8 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class NamingUtil {
 
-	static String subProjectName(final String rootProjectName, String projectName) {
+	static String subProjectName(final String rootProjectName, final String projectName) {
+		final def sanitizedString = RegExUtils.replaceAll(projectName, "[\\\\\\/\\:\\<\\>\\\"\\?\\*\\|]", "_")
 		return "${rootProjectName}_${StringUtils.capitalize(projectName)}"
 	}
 }
